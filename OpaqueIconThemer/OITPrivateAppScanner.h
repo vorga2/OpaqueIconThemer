5e7a96045684702758c52f5eada2491bafdd5181
vorga2 <daniil.sedov.2011@gmail.com>
2026-08-26 19:55:56 +0300

    Add direct bundle lookup fallback

--- OpaqueIconThemer/OITPrivateAppScanner.h
+++ OpaqueIconThemer/OITPrivateAppScanner.h
@@ -14,6 +14,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface OITPrivateAppScanner : NSObject
 + (NSArray<OITInstalledApplication *> *)installedApplications;
++ (nullable OITInstalledApplication *)installedApplicationForBundleIdentifier:(NSString *)bundleIdentifier;
 + (NSString *)scanStatus;
 @end
 
