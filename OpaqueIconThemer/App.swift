4db3c9d41ee43e2d79f29cf31c4d8f62cef86be8
vorga2 <daniil.sedov.2011@gmail.com>
2026-08-26 19:19:19 +0300

    Request tracking permission on first launch

--- OpaqueIconThemer/App.swift
+++ OpaqueIconThemer/App.swift
@@ -1,11 +1,19 @@
 import SwiftUI
+import AppTrackingTransparency
 
 @main
 struct OpaqueIconThemerApp: App {
     @StateObject private var store = ThemeStore()
+
     var body: some Scene {
         WindowGroup {
-            ContentView().environmentObject(store)
+            ContentView()
+                .environmentObject(store)
+                .task {
+                    guard ATTrackingManager.trackingAuthorizationStatus == .notDetermined else { return }
+                    try? await Task.sleep(nanoseconds: 500_000_000)
+                    _ = await ATTrackingManager.requestTrackingAuthorization()
+                }
         }
     }
 }
