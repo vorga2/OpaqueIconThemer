354ff4f70bcd1e3bc9b98d0c6f783461279a9415
vorga2 <daniil.sedov.2011@gmail.com>
2026-08-26 18:01:34 +0300

    Add private installed-app scanner interface

---
+++ OpaqueIconThemer/OITPrivateAppScanner.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OITInstalledApplication : NSObject
+@property (nonatomic, copy) NSString *bundleIdentifier;
+@property (nonatomic, copy) NSString *displayName;
+@property (nonatomic, strong, nullable) UIImage *icon;
+- (instancetype)initWithBundleIdentifier:(NSString *)bundleIdentifier
+                              displayName:(NSString *)displayName
+                                    icon:(nullable UIImage *)icon;
+@end
+
+@interface OITPrivateAppScanner : NSObject
++ (NSArray<OITInstalledApplication *> *)installedApplications;
++ (NSString *)scanStatus;
+@end
+
+NS_ASSUME_NONNULL_END
